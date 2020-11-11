1a8a4fac7803e520adbf0d319b70a38b233e8775
Karol Szafrański <kszafranski@eclipsesource.com>
2020-11-11 15:01:28 +0100

    Make linter fail

--- SwiftLinterTest/ViewController.swift
+++ SwiftLinterTest/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func handling() {
+        try! throwing()
+    }
 
+    func throwing() throws {
+        throw NSError(domain: "a", code: 1, userInfo: nil)
+    }
 }
-
